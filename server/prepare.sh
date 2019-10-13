ab8c4df5eef5cce2b85e8fb7d3183f34ed828511
lganzzzo <lganzzzo@gmail.com>
2019-10-13 23:47:41 +0300

    prepare.sh draft

---
+++ server/prepare.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+cd ../../
+
+apt-get install -y cmake
+apt-get install -y pkg-config
+apt-get install -y build-essential
+
+#####################################################
+
+git clone https://github.com/oatpp/oatpp
+
+cd oatpp
+mkdir build && cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOATPP_BUILD_TESTS=OFF ..
+make install
+
+cd ../../
+
+#####################################################
+
+git clone https://github.com/oatpp/oatpp-libressl
+
+cd oatpp-libressl
+
+./utility/install-deps/install-libressl.sh
+
+mkdir build && cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make install
+
+cd ../../
+
+#####################################################
+
+cd website/server/main
+
+mkdir build && cd build
+
+cmake ..
+make
+
