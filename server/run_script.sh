2b289d393af5e0f2d828969fadd664a243271a3b
lganzzzo <lganzzzo@gmail.com>
2020-01-27 23:28:27 +0700

    Fix run script

--- server/run_script.sh
+++ server/run_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 (while true; do 
-	echo "starting app... `date '+%d.%d.%Y - %H:%M:%S'`" >> log.txt
+	echo "starting app... `date '+%d.%m.%Y - %H:%M:%S'`" >> log.txt
 	./main/build/oatpp-website-exe
 done)
